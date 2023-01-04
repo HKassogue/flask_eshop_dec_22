c3a8f8997aa3d78aa85abda3104fb92a4f639298
Hamidou KASSOGUE <hamidoukass@gmail.com>
2023-01-04 00:04:31 +0000

    Add of table product and DB migration

--- monapp/__init__.py
+++ monapp/__init__.py
@@ -1,2 +1,6 @@
 from flask import Flask
 from .views import app
+from . import models
+
+# Connect sqlalchemy to app
+models.db.init_app(app)
--- monapp/models.py
+++ monapp/models.py
@@ -1,4 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
 from .views import app
 
 # Config options − Make sure you created a ’ config .py’ file .
 app.config.from_object('config')
+
+# création d’un objet de connexion à la base de données
+db = SQLAlchemy(app)
+
+# création du modèle
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    stock = db.Column(db.Integer, nullable=False, default=1)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+
+db.create_all()
