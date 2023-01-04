29b5f3d252f89d365a371ea9009da12dd4cfbbe5
Hamidou KASSOGUE <hamidoukass@gmail.com>
2023-01-04 00:02:01 +0000

    Add of some configurations like DB & jeton

---
+++ monapp/models.py
@@ -0,0 +1,4 @@
+from .views import app
+
+# Config options − Make sure you created a ’ config .py’ file .
+app.config.from_object('config')
--- monapp/views.py
+++ monapp/views.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Config options − Make sure you created a ’ config .py’ file .
+app.config.from_object('config')
+
 @app.route('/')
 def index():
     return render_template('index.html')
